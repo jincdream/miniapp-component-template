0ea2c472fc783b1c91d6aabaa2241862c609506e
锦此 <a275727449@gmail.com>
2020-07-10 17:03:45 +0800

    Update compiler.js
    fix: build error

--- scripts/compiler.js
+++ scripts/compiler.js
@@ -5,54 +5,59 @@ const rename = require('gulp-rename');
 const cleanCss = require('gulp-clean-css');
 const babel = require('gulp-babel');
 const gulpif = require('gulp-if');
+const injectEnvs = require('gulp-inject-envs');
 
 const isProduction = process.env.NODE_ENV === 'production';
-const dist = isProduction ? path.join(__dirname, '../dist') : path.join(__dirname, '../demo/dist');
-const src = path.join(__dirname, '../components');
+const dist = isProduction ? path.join(__dirname, '../output') : path.join(__dirname, '../demo/dist');
+const src = path.join(__dirname, '../src');
 const extTypes = ['ts', 'less', 'json', 'axml', 'sjs'];
+const isRpx = process.argv.splice(2)[0] === '--rpx';
+const env = { jsUnitRpx: isRpx };
 
-gulp.task('less', () => {
-  gulp.src(`${src}/**/*.less`)
-    .pipe(less())
-    .pipe(gulpif(isProduction, cleanCss()))
-    .pipe(rename({
-      extname: '.acss',
-    }))
-    .pipe(gulp.dest(dist));
-});
+gulp.task('less', () => gulp.src(`${src}/**/*.less`)
+  .pipe(less({
+    modifyVars: {
+      '@pixelSize': isRpx ? '1rpx' : '0.5px',
+    },
+  }))
+  .on('error', e => console.error(e))
+  .pipe(gulpif(isProduction, cleanCss()))
+  .pipe(rename({
+    extname: '.acss',
+  }))
+  .pipe(gulp.dest(dist)));
 
-gulp.task('ts', () => {
-  gulp.src(`${src}/**/*.ts`)
-    .pipe(babel())
-    .on('error', (err) => {
-      console.log(err);
-    })
-    .pipe(gulp.dest(dist));
-});
+gulp.task('ts', () => gulp.src(`${src}/**/*.ts`)
+  .pipe(babel())
+  .pipe(injectEnvs(env))
+  .on('error', (err) => {
+    console.log(err);
+  })
+  .pipe(gulp.dest(dist)));
 
-gulp.task('json', () => {
-  gulp.src(`${src}/**/*.json`)
-    .pipe(gulp.dest(dist));
-});
+gulp.task('json', () => gulp.src(`${src}/**/*.json`)
+  .pipe(gulp.dest(dist)));
 
-gulp.task('axml', () => {
-  gulp.src(`${src}/**/*.axml`)
-    .pipe(gulp.dest(dist));
-});
+gulp.task('axml', () => gulp.src(`${src}/**/*.axml`)
+  .pipe(gulp.dest(dist)));
 
-gulp.task('sjs', () => {
-  gulp.src(`${src}/**/*.sjs`)
-    .pipe(gulp.dest(dist));
-});
+gulp.task('sjs', () => gulp.src(`${src}/**/*.sjs`)
+  .pipe(gulp.dest(dist)));
 
-gulp.task('build', extTypes);
-gulp.start('build');
+const build = gulp.series(...extTypes);
+build();
 
 if (!isProduction) {
   extTypes.forEach((type) => {
-    const watcher = gulp.watch(`${src}/**/*` + type, [type]);
+    const watcher = gulp.watch(`${src}/**/*${type}`, gulp.series(type));
     watcher.on('change', (event) => {
-      console.log('File ' + event.path + ' was ' + event.type);
+      console.log(`File ${event} was change`);
+    });
+    watcher.on('add', (event) => {
+      console.log(`File ${event} was add`);
+    });
+    watcher.on('unlink', (event) => {
+      console.log(`File ${event} was remove`);
     });
   });
 }
